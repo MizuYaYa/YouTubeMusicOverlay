6e1174ddd4342b415310f1b8bf0f5a933f770474
MizuYaYa <106097784+MizuYaYa@users.noreply.github.com>
2025-04-03 05:01:22 +0900

    chore: アイコンを変更

--- apps/extension/wxt.config.ts
+++ apps/extension/wxt.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from "wxt";
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   extensionApi: "chrome",
-  modules: ["@wxt-dev/module-solid"],
+  modules: ["@wxt-dev/module-solid", "@wxt-dev/auto-icons"],
   outDir: "dist",
 });
